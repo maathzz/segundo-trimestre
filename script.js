ccbbde009907da7babd808089bbebae5c8b53f41
maathzz <161603184+maathzz@users.noreply.github.com>
2024-05-28 08:49:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const textoResultado= document.querySelector(".texto-resultado");
 
+const perguntas = [
+  {
+    enunciado:"Qual o melhor mapa na opinião do Matheus"
+      alternativas: [
+        "Breeze";
+        "Sunset";
+            ]
+},
+{
+    enunciado:"perguntas 2"
+      alternativas: [
+        "alternativa 1";
+        "alternativa 2";
+            ]
+},
+{
+    enunciado:"perguntas 3"
+      alternativas: [
+        "alternativa 1";
+        "alternativa 2";
+            ]
+},
+{
+    enunciado:"perguntas 4"
+      alternativas: [
+        "alternativa 1";
+        "alternativa 2";
+            ]
+},
+{
+    enunciado:"perguntas 5"
+      alternativas: [
+        "alternativa 1";
+        "alternativa 2";
+            ]
+},
+];
