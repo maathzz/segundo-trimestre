af484878644d7d16e85f13f78c5a5d00909ab90d
maathzz <161603184+maathzz@users.noreply.github.com>
2024-06-11 08:21:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ const perguntas = [
             ]
 },
 {
-    enunciado:"perguntas 3"
+    enunciado:"Qual foi o peak do Matheus?"
       alternativas: [
-        "alternativa 1";
-        "alternativa 2";
+        "Imortal 1";
+        "Ascendente 3";
             ]
 },
 {
@@ -41,3 +41,22 @@ const perguntas = [
             ]
 },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
