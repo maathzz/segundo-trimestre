a1ffddc0b6efe2fe6f7a41cc96df4425825c3e9f
maathzz <161603184+maathzz@users.noreply.github.com>
2024-07-30 10:21:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,111 @@
-const caixaPrincipal= document.querySelector(".caixa-principal");
-const caixaPerguntas= document.querySelector(".caixa-perguntas");
-const caixaAlternativas= document.querySelector(".caixa-alternativas");
-const caixaResultado= document.querySelector(".caixa-resultado");
-const textoResultado= document.querySelector(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado:"Qual o melhor mapa na opinião do Matheus"
+    enunciado: "Qual o melhor mapa na opinião do Matheus?",
       alternativas: [
   {
- texto: "Breeze";
-afirmaçao: "afirmaçao";
+ texto: "Breeze",
+afirmaçao: "afirmaçao"
 }
 {
-  texto: "Sunset";
+  texto: "Sunset",
+afirmaçao: "afirmaçao"
+}
+            ]
+},
+{
+    enunciado:"Qual o agente mais divertido de jogar?",
+      alternativas: [
+        {
+        texto: "Reyna",
+          afirmaçao: "afirmaçao"
+        }
+        {
+        texto: "Jett",
+          afirmaçao: "afirmaçao"
+            ]
+},
+{
+    enunciado:"Qual foi o peak do Matheus?",
+      alternativas: [
+  {
+        texto: "Imortal 2",
+    afirmaçao: "afirmaçao"
+  }
+  {
+        texto: "Ascendente 3",
+          afirmaçao: "afirmaçao"
+            ]
+},
+{
+    enunciado:"Qual o main do Matheus?",
+      alternativas: [
+        {
+        texto: "Jett",
+        afirmaçao: "afirmaçao"
+        }
+        {
+          texto: "Clove",
+          afirmaçao: "afirmaçao"
+        }
+            ]
+},
 
-
-            ]
-},
 {
-    enunciado:"Qual o agente mais divertido de jogar?"
+    enunciado:"Melhor faca do jogo?",
       alternativas: [
-        "Reyna";
-        "Jett";
-            ]
-},
-{
-    enunciado:"Qual foi o peak do Matheus?"
-      alternativas: [
-        "Imortal 1";
-        "Ascendente 3";
-            ]
-},
-{
-    enunciado:"Qual o main do Matheus?"
-      alternativas: [
-        "Jett";
-        "Clove";
-            ]
-},
-{
-    enunciado:"perguntas 5"
-      alternativas: [
-        "alternativa 1";
-        "alternativa 2";
+        {
+        texto: "karambit rgx",
+          afirmaçao: "afirmaçao"
+        }
+        {
+        texto: "butterfly rgx",
+          afirmaçao: "afirmaçao"
+        }
             ]
 },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal="";
 
 function mostraPergunta() {
+  if (atual >= perguntas.length) {
+    return;
+  }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+  caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.text;
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmação
+  historiaFinal += afirmacoes + " ";
+  atual++
+mostraPergunta();
+}
+
+funciton mostraResultado() {
+caixaPerguntas.textContent = " ";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
